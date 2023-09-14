c0fdaeaa55461340380fcf89b377ea7186ab8042
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2023-09-14 15:03:16 +0900

    16장 프로퍼티 어트리뷰트

---
+++ src/Chapter16/ex16-12.js
@@ -0,0 +1,20 @@
+const person = {name: 'Lee'};
+
+console.log(Object.isFrozen(person));
+
+Object.freeze(person);
+
+console.log(Object.isFrozen(person));
+
+console.log(Object.getOwnPropertyDescriptors(person));
+
+person.age = 20;
+console.log(person);
+
+delete person.name;
+console.log(person);
+
+person.name = 'kim';
+console.log(person);
+
+Object.defineProperty(person, 'name',{configurable:true});
