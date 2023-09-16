b74b6b289ea3f71b401cf41774a52e043c3c4c23
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2023-09-16 16:31:42 +0900

    19장 프로토타입

---
+++ src/Chapter19-2/ex19-56.js
@@ -0,0 +1,19 @@
+function Person(name) {
+    this.name = name;
+}
+
+Person.prototype.sayHello = function () {
+    console.log(`Hi! My name is ${this.name}`);
+};
+
+Person.staticProp = 'static prop';
+
+Person.staticMethod = function () {
+    console.log('static method');
+}
+
+const me = new Person("Lee");
+
+Person.staticMethod();
+
+me.staticMethod();
