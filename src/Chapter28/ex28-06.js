4990697f92cd3eed56645eaf0dcf047eb2cb6446
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2023-10-03 22:27:13 +0900

    28장 Number

---
+++ src/Chapter28/ex28-06.js
@@ -0,0 +1,21 @@
+function isEqual(a, b) {
+    return Math.abs(a - b) < Number.EPSILON;
+}
+
+isEqual(0.1 + 0.2 + 0.3);
+
+Number.MAX_VALUE;
+Infinity > Number.MAX_VALUE;
+
+Number.MIN_VALUE;
+Number.MIN_VALUE > 0;
+
+Number.MAX_SAFE_INTEGER;
+
+Number.MIN_SAFE_INTEGER;
+
+Number.POSITIVE_INFINITY;
+
+Number.NEGATIVE_INFINITY;
+
+Number.NaN;
