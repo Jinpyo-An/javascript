adcf22a94b3393badd1447b0ac0a0f41f9a51583
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2023-10-08 13:11:16 +0900

    28장 Number

---
+++ src/Chapter28/ex28-20.js
@@ -0,0 +1,28 @@
+// Number.isSafeInteger(0);
+//
+// Number.isSafeInteger(0.5);
+//
+// Number.isSafeInteger('123');
+//
+// Number.isSafeInteger(false);
+//
+// Number.isSafeInteger(Infinity);
+
+(77.1234).toExponential();
+(77.1234).toExponential(2);
+(77.1234).toExponential(4);
+
+(12345.6789).toFixed();
+(12345.6789).toFixed(1);
+(12345.6789).toFixed(2);
+(12345.6789).toFixed(3);
+
+(12345.6789).toPrecision();
+(12345.6789).toPrecision(1);
+(12345.6789).toPrecision(2);
+(12345.6789).toPrecision(6);
+
+(10).toString();
+(16).toString(2);
+(16).toString(8);
+(16).toString(16);
