861ef52558e7afb47f89b462015a36954bf72003
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2023-10-08 13:32:46 +0900

    29장 Math

---
+++ src/Chapter29/ex29-01.js
@@ -0,0 +1,66 @@
+// Math.PI;
+//
+// Math.abs(-1);
+// Math.abs('-1');
+// Math.abs('');
+// Math.abs([]);
+// Math.abs(null);
+// Math.abs(undefined);
+// Math.abs({});
+// Math.abs('string');
+// Math.abs();
+//
+// Math.round(1.4);
+// Math.round(1.6);
+// Math.round(-1.4);
+// Math.round(-1.6);
+// Math.round(1);
+// Math.round();
+//
+// Math.ceil(1.4);
+// Math.ceil(1.6);
+// Math.ceil(-1.4);
+// Math.ceil(-1.6);
+// Math.ceil(1);
+// Math.ceil();
+//
+// Math.floor(1.9);
+// Math.floor(9.1);
+// Math.floor(-1.9);
+// Math.floor(-9.1);
+// Math.floor(1);
+// Math.floor();
+//
+// Math.sqrt(9);
+// Math.sqrt(-9);
+// Math.sqrt(2);
+// Math.sqrt(1);
+// Math.sqrt(0);
+// Math.sqrt();
+
+// console.log(Math.random());
+// const random = Math.floor((Math.random() * 10) + 1);
+// console.log(random);
+//
+// Math.pow(2, 8);
+// Math.pow(2, -1);
+// Math.pow(2);
+
+// 2 ** 2 ** 2;
+// Math.pow(Math.pow(2, 2), 2);
+
+Math.max(1);
+Math.max(1, 2);
+Math.max(1, 2, 3);
+Math.max();
+
+Math.max.apply(null, [1, 2, 3]);
+Math.max(...[1, 2, 3]);
+
+Math.min(1);
+Math.min(1, 2);
+Math.min(1, 2, 3);
+Math.min();
+
+Math.min.apply(null, [1, 2, 3]);
+Math.min(...[1, 2, 3]);
