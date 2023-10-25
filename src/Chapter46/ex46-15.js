395c66d70bfaa71c902b407726fc09d014153f11
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2023-10-25 19:34:21 +0900

    46장 제너레이터와 async/await

---
+++ src/Chapter46/ex46-15.js
@@ -0,0 +1,22 @@
+async function foo(n) {
+    return n;
+}
+
+foo(1).then(v => console.log(v));
+
+const bar = async function (n) {
+    return n;
+};
+bar(2).then(v => console.log(v));
+
+const baz = async n => n;
+baz(3).then(v => console.log(v));
+
+const obj = {
+    async foo(n) {
+        return n;
+    },
+};
+
+obj.foo(4).then(v => console.log(v));
+
