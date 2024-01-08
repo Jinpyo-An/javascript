e9f9660d05d32b5134722ac154477c4d35f2fd05
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2024-01-08 16:01:37 +0900

    [javascript-deepdive] 스프레드, 구조 분해 할당

---
+++ javascript-remind/chapter35/src/35-1.js
@@ -0,0 +1,15 @@
+// const arr = [1, 2, 3];
+//
+// const max = Math.max(...arr);
+
+// function foo(...rest) {
+//     console.log(rest);
+// }
+//
+// foo(...[1, 2, 3]);
+
+// const arr = [...[1, 2], ...[3, 4]];
+// console.log(arr);
+
+
+
---
+++ javascript-remind/chapter36/src/36-1.js
@@ -0,0 +1,33 @@
+// const arr = [1, 2, 3];
+//
+// const [one, two, three] = arr;
+//
+// console.log(one, two, three);
+//
+// const [a, b, c = 3] = [1, 2];
+// console.log(a, b, c);
+
+// const user = {firstName: "Ungmo", lastName: "Lee"};
+//
+// const {firstName, lastName} = user;
+// console.log(firstName, lastName);
+
+// const str = "Hello";
+//
+// const {length} = str;
+// console.log(length);
+//
+// const todos = {
+//     id: 1,
+//     content: "HTML",
+//     completed: true,
+// };
+//
+// const {id} = todos;
+// console.log(id);
+
+// function printTodo({content, completed}) {
+//     console.log(`할일 ${content}은 ${completed ? "완료" : "비완료"} 상태입니다.`);
+// }
+//
+// printTodo({id: 1, content: "HTML", completed: true});
