d322d1730e96a9b7665b4787d1c08e787be47286
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2024-01-09 15:43:46 +0900

    [javascript-deepdive] 42장 비동기 프로그래밍

---
+++ javascript-remind/chapter42/src/42-1.js
@@ -0,0 +1,42 @@
+// function sleep(func, delay) {
+//     const delayUntill = Date.now() + delay;
+//
+//     while (Date.now() < delayUntill) ;
+//
+//     func();
+// }
+//
+// function foo() {
+//     console.log("foo");
+// }
+//
+// function bar() {
+//     console.log("bar");
+// }
+//
+// sleep(foo, 3 * 1000);
+//
+// bar();
+
+// function foo() {
+//     console.log("foo");
+// }
+//
+// function bar() {
+//     console.log("bar");
+// }
+//
+// setTimeout(foo, 3 * 1000);
+// bar();
+
+// function foo() {
+//     console.log("foo");
+// }
+//
+// function bar() {
+//     console.log("bar");
+// }
+//
+// setTimeout(foo, 0);
+// bar();
+
